af9c773db05f3db742cb59531d1a12f35bfbd641
Thundasantana <ednilson.sdas@outlook.com>
2023-11-14 20:48:49 -0300

    EventService

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,50 +1,31 @@
 package com.devsuperior.bds02.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.bds02.dto.CityDTO;
-import com.devsuperior.bds02.entities.City;
-import com.devsuperior.bds02.repositories.CityRepository;
-import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class EventService {
 	
 	@Autowired
-	private CityRepository repository;
-	
-	@Transactional(readOnly = true)
-	public List<CityDTO> findAll(){
-		List<City> list = repository.findAll(Sort.by("name"));
-		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
-	}
-	
+	private EventRepository repository;
+
 	@Transactional()
-	public CityDTO insert(CityDTO dto) {
-		City entity = new City();
-		entity.setName(dto.getName());
-		entity = repository.save(entity);
-		return new CityDTO(entity);
-	}
-	
-	public void delete(Long id) {
+	public EventDTO update(Long id, EventDTO dto) {
 		try {
-			repository.deleteById(id);
-		} catch (EmptyResultDataAccessException e) {
+			Event entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
-		} catch (DataIntegrityViolationException e) {
-			throw new DatabaseException("Integrity violation");
 		}
-
 	}
-
 }
